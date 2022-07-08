5bceb2fa8c6db703b1a13fed22f34763aa151dec
Ian Agustin <ian.agustin071729@gmail.com>
2022-07-08 09:32:54 +0800

    Added experimental code

--- script.js
+++ script.js
@@ -64,42 +64,58 @@ const GameBoard = (() => {
 
 const PlayerFactory = (player, is_user, difficulty) => {
     const gameBoard = GameBoard.gameBoard;
-    let userTurn = true;
+    // let userTurn = true;
 
+    const getPlayer = () => player;
+    const getIsUser = () => is_user;
+    const getDiff = () => difficulty;
 
-    const moveUser = (event) => {
-        if (userTurn == true) {
-            event.target.textContent = player
-            let userClass = `${player}-mark`
-            event.target.classList.add(userClass);
-            GameBoard.checkForWinner(userClass);
-        }
-    }
-
-    const moveEnemy = (event) => {
-        if (userTurn == false) {
-            event.target.textContent = player
-            let userClass = `${player}-mark`
-            event.target.classList.add(userClass);
-            GameBoard.checkForWinner(userClass);
-        }
-    }
-
-    gameBoard.forEach((cell) => {
-        cell.addEventListener("click", e => {
-            if (player === "x" && is_user) {
-                moveUser(e);
-                userTurn = false;
-            }
-            else if (player === "x" && !is_user) {
-                moveEnemy(e);
-                userTurn = true;
-            }
-
+    const makeTurn = () => {
+        gameBoard.forEach((cell) => {
+            cell.addEventListener("click", e => {
+                e.target.textContent = player;
+                let markClass = `${player}-mark`;
+                e.target.classList.add(markClass);
+                GameBoard.checkForWinner(markClass);
+            })
         })
-    })
+    }
 
+    return {getPlayer, getIsUser, getDiff, makeTurn};
+
+    // const moveUser = (event) => {
+    //     if (userTurn == true) {
+    //         event.target.textContent = player
+    //         let userClass = `${player}-mark`
+    //         event.target.classList.add(userClass);
+    //         GameBoard.checkForWinner(userClass);
+    //     }
+    // }
+
+    // const moveEnemy = (event) => {
+    //     if (userTurn == false) {
+    //         event.target.textContent = player
+    //         event.target.classList.add(userClass);
+    //         GameBoard.checkForWinner(userClass);
+    //     }
+    // }
+
+    // gameBoard.forEach((cell) => {
+    //     cell.addEventListener("click", e => {
+    //         if (player === "x" && is_user) {
+    //             moveUser(e);
+    //             userTurn = false;
+    //         }
+    //         else if (player === "x" && !is_user) {
+    //             moveEnemy(e);
+    //             userTurn = true;
+    //         }
+
+    //     })
+    // })
 }
+
+
 
 
 
